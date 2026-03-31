898bcc1fa4374cb9de59659d38fe18eecf2af0cc
Jose Manuel Oberreuter <jose.manuel.oberreuter@gmail.com>
2026-03-30 22:36:44 -0300

    chore: bump app version to 1.0.1 in app.config.js for release

--- app.config.js
+++ app.config.js
@@ -21,7 +21,7 @@ module.exports = {
   expo: {
     name: 'BencinaBarata',
     slug: 'BencinaBarata',
-    version: '1.0.0',
+    version: '1.0.1',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
